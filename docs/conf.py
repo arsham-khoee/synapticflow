46bd1a0266a8b6c72079adf2be0addfeb1ec5934
Mohammad Mehdi Begmaz <94954298+MohammadMehdi81@users.noreply.github.com>
2023-04-02 00:34:46 +0330

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,10 +14,9 @@ sys.path.append(os.path.abspath("./kitchen-sink/demo_py"))
 # -- Project information -----------------------------------------------------
 #
 
-# project = "SynapticFlow"
-project = ""
-copyright = "2023, Arsham Gholamzadeh, Negar Sourati, Mobin Nesari, Mohammad Mehdi Begmaz"
-author = "Arsham Gholamzadeh, Negar Sourati, Mobin Nesari, Mohammad Mehdi Begmaz"
+project = "SynapticFlow"
+copyright = "2023, Arsham Gholamzadeh Khoee, Negar Sourati, Mobin Nesari, Mohammad Mehdi Begmaz"
+author = "Arsham Gholamzadeh Khoee, Negar Sourati, Mobin Nesari, Mohammad Mehdi Begmaz"
 
 # -- General configuration ---------------------------------------------------
 #
@@ -73,7 +72,7 @@ myst_heading_anchors = 3
 #
 
 html_theme = "furo"
-
+html_title = "SynapticFlow"
 language = "en"
 
 
@@ -85,7 +84,7 @@ html_theme_options: Dict[str, Any] = {
     "footer_icons": [
         {
             "name": "GitHub",
-            "url": "https://github.com/pradyunsg/furo",
+            "url": "https://github.com/arsham-khoee/synapticflow",
             "html": """
                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
@@ -94,16 +93,10 @@ html_theme_options: Dict[str, Any] = {
             "class": "",
         },
     ],
-    "source_repository": "https://github.com/pradyunsg/furo/",
-    "source_branch": "main",
+    "source_repository": "https://github.com/arsham-khoee/synapticflow",
+    "source_branch": "Document",
     "source_directory": "docs/",
 }
-
-if "READTHEDOCS" in os.environ:
-    html_theme_options["announcement"] = (
-        "This documentation is hosted on Read the Docs only for testing. Please use "
-        "<a href='https://pradyunsg.me/furo/'>the main documentation</a> instead."
-    )
 
 # -- Options for theme development -------------------------------------------
 # Make sure these are all set to the default values.
@@ -130,8 +123,8 @@ if RTD_TESTING or "FURO_RTD_TESTING" in os.environ:
     html_context["current_version"] = "latest"
     html_context["conf_py_path"] = "/docs/"
     html_context["display_github"] = True
-    html_context["github_user"] = "pradyunsg"
-    html_context["github_repo"] = "furo"
+    html_context["github_user"] = "Arsham-Khoee"
+    html_context["github_repo"] = "synapticflow"
     html_context["github_version"] = "main"
     html_context["slug"] = "furo"
 
